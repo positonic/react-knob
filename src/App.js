59fa151e422d9d55c398491075bdacf400852d59
James <jamespfarrell@gmail.com>
2019-03-08 14:14:15 +0100

    First working version new structure, with linting and prettier and husky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import ReactKnob from './ReactKnob';
+import React, { Component } from "react";
+import ReactKnob from "./ReactKnob";
 import styled from "styled-components";
 
 class App extends Component {
@@ -8,50 +8,51 @@ class App extends Component {
     this.setVolume = this.setVolume.bind(this);
     this.state = {
       gainValue: 20
-    }
+    };
   }
   setVolume(fatId, property, value) {
-    console.log('sv coluem', value)
+    console.log("sv coluem", value);
     this.setState({
       gainValue: value
-    })
+    });
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            React knob
-          </p><ReactKnob width={45}
-                         height={45}
-                         fatId={1}
-                         setValue={this.setVolume}
-                         value={this.state.gainValue}
-                         defaultValue={20}
-                         unit={1}
-                         max={100}
-                         min={0}
-                         fatProperty={'gain'}/>
+          <p>React knob</p>
+          <ReactKnob
+            width={45}
+            height={45}
+            fatId={1}
+            setValue={this.setVolume}
+            value={this.state.gainValue}
+            defaultValue={20}
+            unit={1}
+            max={100}
+            min={0}
+            fatProperty={"gain"}
+          />
           <h5>{this.state.gainValue}</h5>
         </header>
-
       </div>
     );
   }
 }
 
 export default styled(App)`
-.App {
-  text-align: center;
-}
+  .App {
+    text-align: center;
+  }
 
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}`;
+  .App-header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+`;
